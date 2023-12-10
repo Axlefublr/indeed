5db176d39bdce76422af3ef6f47ded1355b4cd6a
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-12-10 20:44:40 +0800

    empty error message if all the strings are already in the file

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
 use args::Args;
 use clap::Parser;
-use std::error::Error;
 use std::fs;
+use std::process::ExitCode;
 
 mod args;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> ExitCode {
     let Args { path, mut strings } = Args::parse();
-    let mut contents: Vec<String> = fs::read_to_string(&path)?
-        .trim_end()
-        .lines()
-        .map(|line| line.to_owned())
-        .collect();
+    let contents = match fs::read_to_string(&path) {
+        Ok(value) => value,
+        Err(error) => {
+            eprintln!("{}", error);
+            return ExitCode::FAILURE;
+        }
+    };
+    let mut contents: Vec<String> = contents.trim_end().lines().map(|line| line.to_owned()).collect();
     strings.retain(|string| !contents.contains(string));
+    let lines = contents.len();
     contents.extend(strings);
-    fs::write(path, contents.join("\n"))?;
-    Ok(())
+    if contents.len() != lines {
+        if let Err(error) = fs::write(path, contents.join("\n")) {
+            eprintln!("{}", error);
+            return ExitCode::FAILURE;
+        }
+        return ExitCode::SUCCESS;
+    }
+    ExitCode::FAILURE
 }
